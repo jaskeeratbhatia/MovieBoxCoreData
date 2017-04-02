2d35ead5b94fd83fbcd9c0ed228ef90a1fb0f7e0
Jaskeerat Singh Bhatia <Jaskeerat@jaskeerats-macbook-pro.local>
2017-04-02 04:44:02 -0400

    Initial Commit

---
+++ MovieBoxCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieBoxCoreData
+//
+//  Created by Jaskeerat Singh Bhatia on 2017-04-02.
+//  Copyright © 2017 Jaskeerat Singh Bhatia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
